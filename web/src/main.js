6e8fd2c1ee03cb2b62b57f6eefaeaab8d377f9f1
unknown <junhui.zou@wenba100.com>
2017-12-08 00:03:35 +0800

    AddStu.vue

--- web/src/main.js
+++ web/src/main.js
@@ -2,10 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
+import VueRouter from 'vue-router'
+import AddStu from './components/AddStu'
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: AddStu },
+]
+
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
