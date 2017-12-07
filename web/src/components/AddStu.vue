5ac13fb16eb91ab48d2d1327e615a0d94a1df3dd
unknown <junhui.zou@wenba100.com>
2017-12-08 00:41:16 +0800

    提交学生数据

--- web/src/components/AddStu.vue
+++ web/src/components/AddStu.vue
@@ -1,14 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essentdsfdsfdsfdsfial Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-    </ul>
+  <div class="container">
+    <form>
+        <div class="form-group">
+            <label for="">name</label>
+            <input type="text" class="form-control" id="name" placeholder="name">
+        </div>
+        <div class="form-group">
+            <label for="">手机</label>
+            <input type="phone" class="form-control" id="phone" placeholder="phone">
+        </div>
+        <div class="form-group">
+            <label for="">QQ</label>
+            <input type="qq" class="form-control" id="qq" placeholder="qq">
+        </div>
+        <div class="form-group">
+            <label for="">微信</label>
+            <input type="we-chat" class="form-control" id="we-chat" placeholder="we-chat">
+        </div>
+        <div class="form-group">
+            <label for="">报考学校</label>
+            <select class="form-control">
+                <option>上海大学</option>
+                <option>中山大学</option>
+                <option>南京大学</option>
+            </select>
+          </div>
+        <div class="form-group">
+            <label for="">报考专业</label>
+            <select class="form-control">
+                <option>通信工程</option>
+                <option>计算机</option>
+                <option>法学</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="">年级</label>
+            <select class="form-control">
+                <option>2019</option>
+                <option>2020</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="">需求</label>
+            <textarea class="form-control" rows="3"></textarea>
+        </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
   </div>
 </template>
 
