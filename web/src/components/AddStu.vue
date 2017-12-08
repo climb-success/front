0e05a487f0204d34b0d12f0ae715d94053e657a8
zoujunhui1 <junhuizoujh@163.com>
2017-12-08 14:08:14 +0800

    addvue优化

--- web/src/components/AddStu.vue
+++ web/src/components/AddStu.vue
@@ -1,79 +1,75 @@
 <template>
-  <div class="container">
-    <form>
+  <div class="container col-md-9 col-md-offset-2">
+    <form class="form-horizontal col-md-5 col-md-offset-2">
         <div class="form-group">
-            <label for="">name</label>
-            <input type="text" class="form-control" id="name" placeholder="name">
+            <div class="input-group">
+                <span class="input-group-addon">名字：</span>
+                <input type="text" class="form-control" id="name" placeholder="名字">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon">手机：</span>
+                <input type="text" class="form-control" id="phone" placeholder="手机">
+            </div>
         </div>
         <div class="form-group">
-            <label for="">手机</label>
-            <input type="phone" class="form-control" id="phone" placeholder="phone">
+            <div class="input-group">
+                <span class="input-group-addon">QQ：</span>
+                <input type="qq" class="form-control" id="qq" placeholder="QQ">
+            </div>
         </div>
         <div class="form-group">
-            <label for="">QQ</label>
-            <input type="qq" class="form-control" id="qq" placeholder="qq">
+            <div class="input-group">
+                <span class="input-group-addon">微信：</span>
+                <input type="text" class="form-control" id="we-chat" placeholder="微信">
+            </div>
         </div>
         <div class="form-group">
-            <label for="">微信</label>
-            <input type="we-chat" class="form-control" id="we-chat" placeholder="we-chat">
+            <div class="input-group">
+                <span class="input-group-addon">报考学校：</span>
+                <select class="form-control" id="school">
+                    <option>上海大学</option>
+                    <option>中山大学</option>
+                    <option>南京大学</option>
+                </select>
+            </div>
         </div>
         <div class="form-group">
-            <label for="">报考学校</label>
-            <select class="form-control">
-                <option>上海大学</option>
-                <option>中山大学</option>
-                <option>南京大学</option>
-            </select>
-          </div>
-        <div class="form-group">
-            <label for="">报考专业</label>
-            <select class="form-control">
-                <option>通信工程</option>
-                <option>计算机</option>
-                <option>法学</option>
-            </select>
+            <div class="input-group">
+                <span class="input-group-addon">报考专业：</span>
+                <select class="form-control" id="profession" >
+                    <option>通信工程</option>
+                    <option>计算机</option>
+                    <option>法学</option>
+                </select>
+            </div>
         </div>
         <div class="form-group">
-            <label for="">年级</label>
-            <select class="form-control">
-                <option>2019</option>
-                <option>2020</option>
-            </select>
+            <div class="input-group">
+                <span class="input-group-addon">年级：</span>
+                <select class="form-control" id="grade">
+                    <option>2019</option>
+                    <option>2020</option>
+                </select>
+            </div>
         </div>
         <div class="form-group">
-            <label for="">需求</label>
-            <textarea class="form-control" rows="3"></textarea>
+            <div class="input-group">
+                <span class="input-group-addon">需求：</span>
+                <textarea class="form-control" rows="2" id="requirement"></textarea>
+            </div>
         </div>
-      <button type="submit" class="btn btn-default">Submit</button>
+      <button type="submit" class="btn btn-default">提交</button>
     </form>
+      <div class="alert alert-success col-md-5 col-md-offset-2">提交成功</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AddStu',
-  data () {
-    return {
-      msg: 'Adddddd'
-    }
-  }
+
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- web/src/main.js
+++ web/src/main.js
@@ -4,20 +4,23 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 import AddStu from './components/AddStu'
-Vue.use(VueRouter)
-Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
 
 const routes = [
-  { path: '/', component: AddStu },
-]
+  { path: '/', component: AddStu }
+];
 
 const router = new VueRouter({
-  routes // （缩写）相当于 routes: routes
-})
+    routes // （缩写）相当于 routes: routes
+});
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App },
+    render: h => h(App)
+});
