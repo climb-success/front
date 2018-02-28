0a7d4a1e2d85ca265f363e8524a7e06b537f3a3a
zoujunhui1 <junhuizoujh@163.com>
2018-02-28 18:05:16 +0800

    学生提交信息

--- web/src/components/AddStu.vue
+++ web/src/components/AddStu.vue
@@ -4,63 +4,63 @@
         <div class="form-group">
             <div class="input-group">
                 <span class="input-group-addon">名字：</span>
-                <input type="text" class="form-control" id="name" placeholder="名字">
+                <input type="text" class="form-control" id="name" v-model="userInfo.name" placeholder="名字">
             </div>
         </div>
 
         <div class="form-group">
             <div class="input-group">
                 <span class="input-group-addon">手机：</span>
-                <input type="text" class="form-control" id="phone" placeholder="手机">
+                <input type="text" class="form-control" id="telePhone" placeholder="手机" v-model="userInfo.telePhone">
             </div>
         </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">QQ：</span>
-                <input type="text" class="form-control" id="qq" placeholder="QQ">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">微信：</span>
-                <input type="text" class="form-control" id="we-chat" placeholder="微信">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">报考学校：</span>
-                <select class="form-control" id="school">
-                    <option>上海大学</option>
-                    <option>中山大学</option>
-                    <option>南京大学</option>
-                </select>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">报考专业：</span>
-                <select class="form-control" id="profession" >
-                    <option>通信工程</option>
-                    <option>计算机</option>
-                    <option>法学</option>
-                </select>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">年级：</span>
-                <select class="form-control" id="grade">
-                    <option>2019</option>
-                    <option>2020</option>
-                </select>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">需求：</span>
-                <textarea class="form-control" rows="2" id="requirement"></textarea>
-            </div>
-        </div>
+        <!--<div class="form-group">-->
+            <!--<div class="input-group">-->
+                <!--<span class="input-group-addon">QQ：</span>-->
+                <!--<input type="text" class="form-control" id="qq" placeholder="QQ">-->
+            <!--</div>-->
+        <!--</div>-->
+        <!--<div class="form-group">-->
+            <!--<div class="input-group">-->
+                <!--<span class="input-group-addon">微信：</span>-->
+                <!--<input type="text" class="form-control" id="we-chat" placeholder="微信">-->
+            <!--</div>-->
+        <!--</div>-->
+        <!--<div class="form-group">-->
+            <!--<div class="input-group">-->
+                <!--<span class="input-group-addon">报考学校：</span>-->
+                <!--<select class="form-control" id="school" v-model="userInfo.schoolId">-->
+                    <!--<option value="1">上海大学</option>-->
+                    <!--<option value="2">中山大学</option>-->
+                    <!--<option value="3">南京大学</option>-->
+                <!--</select>-->
+            <!--</div>-->
+        <!--</div>-->
+        <!--<div class="form-group">-->
+            <!--<div class="input-group">-->
+                <!--<span class="input-group-addon">报考专业：</span>-->
+                <!--<select class="form-control" id="professionalId" v-model="userInfo.professionalId" >-->
+                    <!--<option value="1">通信工程</option>-->
+                    <!--<option value="2">计算机</option>-->
+                    <!--<option value="3">法学</option>-->
+                <!--</select>-->
+            <!--</div>-->
+        <!--</div>-->
+        <!--<div class="form-group">-->
+            <!--<div class="input-group">-->
+                <!--<span class="input-group-addon">年级：</span>-->
+                <!--<select class="form-control" id="grade">-->
+                    <!--<option>2019</option>-->
+                    <!--<option>2020</option>-->
+                <!--</select>-->
+            <!--</div>-->
+        <!--</div>-->
+        <!--<div class="form-group">-->
+            <!--<div class="input-group">-->
+                <!--<span class="input-group-addon">需求：</span>-->
+                <!--<textarea class="form-control" rows="2" id="requirement" v-model="userInfo.requirement"></textarea>-->
+            <!--</div>-->
+        <!--</div>-->
       <button type="submit" class="btn btn-default" v-on:click="submit()">提交</button>
     </form>
       <div class="alert alert-success col-md-5 col-md-offset-2">提交成功</div>
@@ -70,21 +70,22 @@
 <script>
 export default {
     name: 'AddStu',
-    methods:{
-        submit(){
-            this.$http.post('http://39.104.60.7:8080/success/student/updateStudent',
-                    {
-                        firstName: 'Fred',
-                        lastName: 'Flintstone'
-                    })
-                    .then(function (response) {
-                        console.log(response);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
+    data(){
+        return {
+            userInfo:{id:0,name:null,telePhone:null,schoolId:0,professionalId:0}
         }
     },
+    methods:{
+        submit(){
+            this.$http.post('http://39.104.60.7:8080/success/student/updateStudent', this.userInfo)
+                .then(function (response) {
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    }
 
 }
 </script>
