c393bf5d3ae4e173b90839174e31f466874cd920
Wenba <junhui.zou@wenba100.com>
2018-03-16 18:40:37 +0800

    背景图片

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,3 +9,9 @@ export default {
     name: 'app',
 }
 </script>
+<style>
+  html,body,#app {
+    width: 100%;
+    height: 100%;
+  }
+</style>
--- web/src/components/AddStu.vue
+++ web/src/components/AddStu.vue
@@ -1,50 +1,52 @@
 <template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
-  <div class="container col-md-9 col-md-offset-2">
-    <form class="form-horizontal col-md-5 col-md-offset-2">
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">名字：</span>
-                <input type="text" class="form-control" id="name" v-model="userInfo.name" placeholder="名字">
-            </div>
-        </div>
+    <div class="row stu">
+        <div class="container col-md-9 col-md-offset-2">
+            <form class="form-horizontal col-md-5 col-md-offset-2">
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">名字：</span>
+                        <input type="text" class="form-control" id="name" v-model="userInfo.name" placeholder="名字">
+                    </div>
+                </div>
 
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">手机：</span>
-                <input type="text" class="form-control" id="telePhone" placeholder="手机" v-model="userInfo.telePhone">
-            </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">手机：</span>
+                        <input type="text" class="form-control" id="telePhone" placeholder="手机" v-model="userInfo.telePhone">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">地区：</span>
+                        <select class="form-control" id="province" v-on:change="school" v-model="provinceId">
+                            <option disabled value="">请选择</option>
+                            <option v-for="(provinces,id) in provinceData" :value="id" >{{ provinces }}</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">报考学校：</span>
+                        <select class="form-control" id="school" v-on:change="profession" v-model="userInfo.schoolId">
+                            <option disabled value="0">请选择</option>
+                            <option v-for="schools in schoolData" :value="schools.id">{{schools.name}}</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">报考专业：</span>
+                        <select class="form-control" id="professionalId" v-model="userInfo.professionalId" >
+                            <option disabled value="0">请选择</option>
+                            <option v-for="professions in professionData.professionals" :value="professions.id">{{ professions.name }}</option>
+                        </select>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-default" v-on:click="submit()">提交</button>
+            </form>
+            <div class="alert alert-success col-md-5 col-md-offset-2" v-bind:class="{hidden: isHidden}">提交成功</div>
         </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">地区：</span>
-                <select class="form-control" id="province" v-on:change="school" v-model="provinceId">
-                    <option disabled value="">请选择</option>
-                    <option v-for="(provinces,id) in provinceData" :value="id" >{{ provinces }}</option>
-                </select>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">报考学校：</span>
-                <select class="form-control" id="school" v-on:change="profession" v-model="userInfo.schoolId">
-                    <option disabled value="0">请选择</option>
-                    <option v-for="schools in schoolData" :value="schools.id">{{schools.name}}</option>
-                </select>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-group">
-                <span class="input-group-addon">报考专业：</span>
-                <select class="form-control" id="professionalId" v-model="userInfo.professionalId" >
-                    <option disabled value="0">请选择</option>
-                    <option v-for="professions in professionData.professionals" :value="professions.id">{{ professions.name }}</option>
-                </select>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-default" v-on:click="submit()">提交</button>
-    </form>
-      <div class="alert alert-success col-md-5 col-md-offset-2" v-bind:class="{hidden: isHidden}">提交成功</div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -110,7 +112,13 @@ export default {
 }
 </script>
 <style>
-    body {
-        background-image: url("../../static/img/1v1.jpg") ;
+    .stu {
+        width: 100%;
+        height: 100%;
+        background-image: url("../../static/img/1v1.jpg");
+        background-size: 100% auto;
+        background-repeat: no-repeat;
+        background-position: center;
     }
+
 </style>
